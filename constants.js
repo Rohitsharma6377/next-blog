b26227e6c4cee4f70d0f5bfdeb320753f0fd7d45
Rohit Sharma <lavish637728@gmail.com>
2024-10-11 15:30:23 +0530

    sec commit

--- constants.js
+++ constants.js
@@ -1,28 +1,28 @@
 //Navbar
 export const NavItems = [
     { id: 1, name: "Home", url: "/" },
-    { id: 2, name: "Services", url: "" },
-    { id: 3, name: "Calculators", url: "" },
-    { id: 5, name: "FAQs", url: "/faq" },
+    // { id: 2, name: "Services", url: "" },
+    // { id: 3, name: "Calculators", url: "" },
+    // { id: 5, name: "FAQs", url: "/faq" },
     { id: 6, name: "Blogs", url: "/blogs" }
 ];
 
   
-export const NavDropdownItems = {
-    services: [
-        { name: "New Home Loan", url: "/new-home-loan" },
-        { name: "Balance Transfer of Existing Loan", url: "/balance-transfer-existing-of-loan" },
-        { name: "Loan Against Property", url: "/loan-against-property" },
-        { name: "Home Construction Loan", url: "/home-construction-loan" },
-        { name: "Home Improvement Loan", url: "/home-improvement-loan" },
-        { name: "Plot Loan", url: "/plot-loan" }
-    ],
+// export const NavDropdownItems = {
+//     services: [
+//         { name: "New Home Loan", url: "/new-home-loan" },
+//         { name: "Balance Transfer of Existing Loan", url: "/balance-transfer-existing-of-loan" },
+//         { name: "Loan Against Property", url: "/loan-against-property" },
+//         { name: "Home Construction Loan", url: "/home-construction-loan" },
+//         { name: "Home Improvement Loan", url: "/home-improvement-loan" },
+//         { name: "Plot Loan", url: "/plot-loan" }
+//     ],
 
-    calculators: [
-        { name: "EMI Calculator", url: "/emi-calculator" },
-        { name: "Eligibility Calculator", url: "/eligibility-calculator" }
-    ]
-};
+//     calculators: [
+//         { name: "EMI Calculator", url: "/emi-calculator" },
+//         { name: "Eligibility Calculator", url: "/eligibility-calculator" }
+//     ]
+// };
 
 
 //SideBar Menu
