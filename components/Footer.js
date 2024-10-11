d3b0c6cdd402501bfd6b1c8b3d48717fc889ed6e
Rohit Sharma <lavish637728@gmail.com>
2024-10-11 15:48:49 +0530

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
                         <div className="col-span-4 mb-5">
                             <h3 className="text-lg font-bold mb-4 text-left">Important Links</h3>
                             <ul className="space-y-2 text-center">
-                                {['Refer a Friend', 'Privacy Policy', 'Terms of use', 'Disclaimer', 'Terms for Cashback'].map((link) => (
+                                {['Privacy Policy', 'Terms of use', 'Disclaimer', 'Terms for Cashback'].map((link) => (
                                     <li key={link}>
                                         <Link href={`/${link.toLowerCase().replace(/ /g, '-')}`} className="text-blue-400 hover:underline">{link}</Link>
                                     </li>
@@ -26,14 +26,14 @@ const Footer = () => {
                         <div className="col-span-4 mb-5">
                             <h3 className="text-lg font-bold mb-4 text-left">Pages</h3>
                             <ul className="space-y-2 text-center">
-                                {['Home', 'About Us', 'Refer a Friend', 'FAQ', 'Blogs', 'Contact Us'].map((link) => (
+                                {['Home', 'About Us', 'FAQ', 'Blogs', 'Contact Us'].map((link) => (
                                     <li key={link}>
                                         <Link href={`/${link.toLowerCase().replace(/ /g, '-')}`} className="text-blue-400 hover:underline">{link}</Link>
                                     </li>
                                 ))}
                             </ul>
                         </div>
-                        {/* Our Products */}
+                        {/* Our Products
                         <div className="col-span-4 mb-5">
                             <h3 className="text-lg font-bold mb-4 text-left">Our Products</h3>
                             <ul className="space-y-2 text-center">
@@ -43,26 +43,26 @@ const Footer = () => {
                                     </li>
                                 ))}
                             </ul>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div className="container mx-auto text-center mt-8">
-                    <p className="text-sm">Copyright © 2020. All rights reserved.</p>
+                    <p className="text-sm">Copyright © 2024. All rights reserved.</p>
                 </div>
             </footer>
 
-            {/* Callback Button */}
+            {/* Callback Button
             <div id="callback" className="fixed bottom-4 right-8 z-10">
                 <button onClick={openForm} className="flex items-center bg-blue-600 text-white p-2 rounded-full shadow-lg transition transform hover:scale-105">
                     <Image src="/images/icons/call-white.svg" alt="Hand" width={24} height={24} className='w-8' />
                     <span className="ml-2 text-sm">Get a Call Back</span>
                 </button>
-            </div>
+            </div> */}
 
             <ContactForm />
 
             {/* WhatsApp and Call Buttons */}
-            <Link
+            {/* <Link
                 target="_blank"
                 href="https://api.whatsapp.com/send?phone=919625100737&amp;text= Hi, I got your number from Trueloans Website."
                 className="fixed bottom-4 left-4 p-3 bg-red-400 rounded-full shadow-lg text-white transition transform hover:scale-105"
@@ -74,7 +74,7 @@ const Footer = () => {
                 className="fixed bottom-4 left-20 p-3 bg-blue-500 rounded-full shadow-lg text-white transition transform hover:scale-105"
             >
                 <Image src="/images/icons/call-button.svg" alt="Call Trueloans Now" width={24} height={24} className='w-6' />
-            </Link>
+            </Link> */}
         </div>
     );
 };
